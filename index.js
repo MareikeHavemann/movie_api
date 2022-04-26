a4dfc4ac31cf5e18f518d89357d3e20eba9f2a91
MareikeHavemann <tipptipptapp@yahoo.de>
2022-04-26 18:43:08 +0200

    Added function showing documentation

--- index.js
+++ index.js
@@ -55,6 +55,10 @@ app.get("/movies", (req, res) => {
   res.json(topMovies);
 });
 
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
 app.get("/", (req, res) => {
   res.send("Welcome to my movies club!");
 });
